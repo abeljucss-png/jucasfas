225d15ecc514058516531b3626aac204bb044dd0
abeljucss-png <abeljucss@gmail.com>
2026-09-10 00:02:29 -0300

    feat(analytics): prepare Google Ads conversion configuration

--- public/analytics-config.js
+++ public/analytics-config.js
@@ -1,5 +1,7 @@
 window.OPINIA_REAL_ANALYTICS = Object.assign({
   ga4MeasurementId: '',
   gtmContainerId: '',
+  googleAdsConversionId: '',
+  googleAdsConversionLabel: '',
   debug: false
 }, window.OPINIA_REAL_ANALYTICS || {});
